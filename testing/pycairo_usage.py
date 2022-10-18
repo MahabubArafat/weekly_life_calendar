d5582f302281cf6ee9654f135e58b763efd75b77
MahabubArafat <arafatmahabub37@gmail.com>
2022-10-18 16:32:54 +0600

    made square with cairo

--- testing/pycairo_usage.py
+++ testing/pycairo_usage.py
@@ -21,21 +21,33 @@ context.rectangle(0, 0, 1, 1)
 context.set_source(pat)
 context.fill()
 
-context.translate(0.1, 0.1)
+context.translate(0.1, 0.1) #mane koi theke drawing start
 
 context.move_to(0, 0)
 
-context.arc(0.2, 0.1, 0.1, -math.pi / 2, 0)
+# context.arc(0.2, 0.1, 0.1, -math.pi / 2, 0)
 # Arc(cx, cy, radius, start_angle, stop_angle)
 
-context.line_to(0.5, 0.1)  # Line to (x,y)
+# context.line_to(0.5, 0.1)  # Line to (x,y)
 
 # Curve(x1, y1, x2, y2, x3, y3)
-context.curve_to(0.5, 0.2, 0.5, 0.4, 0.2, 0.8)
+# context.curve_to(0.5, 0.2, 0.5, 0.4, 0.2, 0.8)
+# context.close_path()
+
+
+#making a square
+context.line_to(0.5, 0)
+context.line_to(0.5, 0.5)
+context.line_to(0, 0.5)
 context.close_path()
+context.line_to(0.5, 0.5)
+context.move_to(0.5, 0)
+context.line_to(0, 0.5)
+context.arc(0.5, 0.5, 0.25, 0.25, 0)
 
 
-context.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
+
+context.set_source_rgb(0.0, 1.0, 0.16)  # Solid color
 context.set_line_width(0.02)
 context.stroke()
 
